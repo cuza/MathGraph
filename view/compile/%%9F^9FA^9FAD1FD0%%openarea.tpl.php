e5edcd6c8a91d3bd8bce69de3a7ed96f6afdf20a
Dave Cuza <dave.cuza90@gmail.com>
2013-11-24 11:59:37 -0500

    1.06

--- view/compile/%%9F^9FA^9FAD1FD0%%openarea.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2013-05-31 04:43:16
-         compiled from openarea.tpl */
-?>
-<div>
-
-
--- view/compile/%%E4^E42^E42F140F%%closepage.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2013-05-31 04:39:37
-         compiled from closepage.tpl */
-?>
-
-</body>
-</html>
