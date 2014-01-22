dae74c2693f882a4df02d752ecfbaa2ae543c113
Osiel Jo <osiel92jt@gmail.com>
2014-01-22 08:51:02 -0500

    added button 4 resetting zoom

--- view/js/scripts/util.js
+++ view/js/scripts/util.js
@@ -70,7 +70,7 @@ function Ver(tipo, item) {
         "</div>" +
         "<div id=\"chart1\" class=\"simple-chart-2\" style=\"height: 230px; width:450px;\">" +
         "</div>" +
-        "<button value=\"reset\" type=\"button\" onclick=\"plot2.resetZoom();\">Reset Zoom</button>" +
+        "<button value=\"reset\" type=\"button\" onclick=\"plot2.resetZoom();\">Cancelar Zoom</button>" +
         "</div>"
     $("#dialog-ui").html(graph_area);
     //Dialog Modal
